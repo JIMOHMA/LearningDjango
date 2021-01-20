b9b1086c90ad2bb06a241520cd54b6f794992763
ayodele <techwithayodele@outlook.com>
2021-01-20 15:30:25 -0500

    Pages, Form, Model and Form Validation

--- Django/Project-Env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\mail4\desktop\career\python projects\django\project-env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
